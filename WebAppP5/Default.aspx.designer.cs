2b21440967ecd2e7d3f5c8fa71839edb6844065b
Larissa Pokam <pokamlarissa@gmail.com>
2020-11-24 18:06:53 -0700

    Edited the Default.aspx to integrate the service.

--- WebAppP5/Default.aspx.designer.cs
+++ WebAppP5/Default.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace WebAppP5
         protected global::System.Web.UI.WebControls.Label lblOutput;
 
         /// <summary>
-        /// Label5 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label5;
-
-        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
@@ -202,15 +193,6 @@ namespace WebAppP5
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
-
-        /// <summary>
-        /// Button4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button4;
 
         /// <summary>
         /// lblmessage control.
